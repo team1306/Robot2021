a02805b9caa330509362841210f21f9a9adf7fe0
martreb <bertwolle3@gmail.com>
2020-09-20 15:57:30 -0500

    some piston stuff. still need to figure out user input better

--- src/main/java/frc/robot/commands/IntakeCommand.java
+++ src/main/java/frc/robot/commands/IntakeCommand.java
@@ -5,7 +5,7 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.utils.UserAnalog;
 
 /**
- * An example command that uses an example subsystem.
+ * The Intake command for the Intake subsystem
  */
 public class IntakeCommand extends CommandBase {
     private final UserAnalog speed;
@@ -17,7 +17,9 @@ public class IntakeCommand extends CommandBase {
     public IntakeCommand(UserAnalog speed, Intake intake) {
         this.speed = speed;
         this.intake = intake;
-        // Use addRequirements() here to declare subsystem dependencies.
+
+        addRequirements(intake);
+        intake.setDefaultCommand(this);
     }
 
     // Called when the command is initially scheduled.
@@ -28,13 +30,28 @@ public class IntakeCommand extends CommandBase {
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
+        // This will just run both motors at the same speed.
         intake.spin(speed.get());
+
+        /*
+         * I will want to add a thing to spit out the balls if need be. I don't think we
+         * have and sensor stuff to count how many balls are in the index, maybe we do,
+         * but I'd have to find it. The method to determine if the balls are jammed
+         * should probably go in the Intake subsystem I think.
+         */
+
+        /*
+         * I will also have to figure out how to retract the intake with this command
+         */
+
+        intake.extend();
 
     }
 
     // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {
+        // Will just stop the spinning of the intake motors.
         intake.spin(0.0);
     }
 
--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -6,6 +6,8 @@ import com.revrobotics.EncoderType;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.utils.Encoder;
@@ -16,12 +18,19 @@ public class Intake extends SubsystemBase {
   private final CANSparkMax motorRight;
   private final CANSparkMax motorLeft;
 
+  // The encoders for the two intake motors
   private final CANEncoder rightEnc;
   private final CANEncoder leftEnc;
 
+  // Just the Encoder that egan wrote. This is here for it's methods.
   private final Encoder enc = Encoder.Grayhill256;
 
-  // TODO pistons on recad #1 - digital input because they go up and down
+  // piston
+  private final DoubleSolenoid intakeArm;
+
+  // piston direction values
+  private static final Value ExtensionDirection = Value.kReverse;
+  private static final Value RetractionDirection = Value.kForward;
 
   /**
    * Creates a new Intake subsystem.
@@ -45,6 +54,11 @@ public class Intake extends SubsystemBase {
     // encoders
     rightEnc = motorRight.getEncoder(EncoderType.kHallSensor, (int) enc.rotationsToPulses(1));
     leftEnc = motorLeft.getEncoder(EncoderType.kHallSensor, (int) enc.rotationsToPulses(1));
+
+    // The piston stuff
+    intakeArm = new DoubleSolenoid(Constants.K_INTAKE_SOLENOID_UP, Constants.K_INTAKE_SOLENOID_DOWN);
+    register();
+
   }
 
   /**
@@ -75,8 +89,30 @@ public class Intake extends SubsystemBase {
     // This method will be called once per scheduler run (vision stuff)
   }
 
-  public double getRPM() {
-    return enc.getVelocity();
+  /*
+   * These two get the RPM of each motor on the intake, in case you need those
+   * numbers
+   */
+  public double getRightRPM() {
+    return rightEnc.getVelocity();
+  }
+
+  public double getLeftRPM() {
+    return leftEnc.getVelocity();
+  }
+
+  /**
+   * Retracts the intake
+   */
+  public void retract() {
+    intakeArm.set(RetractionDirection);
+  }
+
+  /**
+   * Extends the intake
+   */
+  public void extend() {
+    intakeArm.set(ExtensionDirection);
   }
 
 }
