2cdc688172ab3ce0ba8935a11177a884712183d6
emly <emily2004.yao@gmail.com>
2020-09-20 19:20:16 -0500

    climber + spinner cleanup

--- src/main/java/frc/robot/commands/IndexCommand.java
+++ src/main/java/frc/robot/commands/IndexCommand.java
@@ -43,7 +43,7 @@ public class IndexCommand extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    
+
   }
 
   // Returns true when the command should end.
