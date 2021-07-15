624cd152caaec0c55c1dd622de3cb52da8b9055b
Aidan Simkin <Aidan Simkin@DESKTOP-KKAQLRK>
2021-07-14 19:01:19 -0500

    Removing old code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,29 +7,11 @@
 
 package frc.robot;
 
-import frc.robot.commands.UserSwerveDrive;
-
-import java.io.IOException;
-import java.nio.file.Path;
-
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.controller.PIDController;
-import edu.wpi.first.wpilibj.controller.ProfiledPIDController;
-import edu.wpi.first.wpilibj.controller.RamseteController;
-import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
-import edu.wpi.first.wpilibj.trajectory.Trajectory;
-import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
-import edu.wpi.first.wpilibj.trajectory.constraint.DifferentialDriveVoltageConstraint;
-import edu.wpi.first.wpilibj.trajectory.constraint.SwerveDriveKinematicsConstraint;
-import edu.wpi.first.wpilibj.trajectory.TrajectoryUtil;
-import edu.wpi.first.wpilibj.trajectory.TrapezoidProfile;
-import frc.robot.subsystems.SwerveDrive;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.RamseteCommand;
-import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
+import frc.robot.commands.UserSwerveDrive;
+import frc.robot.subsystems.SwerveDrive;
 import frc.robot.utils.Controller;
 import frc.robot.utils.UserAnalog;
 import frc.robot.utils.UserDigital;
@@ -93,56 +75,6 @@ public class RobotContainer {
      * @return the command to run in autonomous
      */
     public Command getAutonomousCommand() {
-        // // Create a speed constraint to ensure we don't accelerate too fast
-        // SwerveDriveKinematicsConstraint autoConstraint  = new SwerveDriveKinematicsConstraint(userSwerveDrive.m_swerveDrive.kinematics, Constants.FASTEST_SPEED_METERS);
-        
-        // // Create config for trajectory
-        // TrajectoryConfig config =
-        // new TrajectoryConfig(Constants.FASTEST_SPEED_METERS, 
-        //     Constants.FASTEST_ACCELERATION)
-        // // Add kinematics to ensure max speed is actually obeyed
-        // .setKinematics(userSwerveDrive.m_swerveDrive.kinematics)  
-        // // Apply the voltage constraint
-        // .addConstraint(autoConstraint);
-
-        // String trajectoryJSON = "../deploy/path/Barrel.wpilib.json";
-        // Trajectory trajectory = new Trajectory(null); // todo what is trajectory list?????
-        // try {
-        //     Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(trajectoryJSON);
-        //     trajectory = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
-        // } catch (IOException ex) {
-        //     System.out.println("Unable to open trajectory");
-        // }
-
-        // TrapezoidProfile.Constraints kThetaControllerConstraints = new TrapezoidProfile.Constraints(
-        //     Constants.kMaxAngularSpeedRadians, Constants.kMaxAngularVelocityRadians);
-  
-        // var thetaController =
-        // new ProfiledPIDController(
-        //     Constants.THETA_CONTROLLER_P, Constants.THETA_CONTROLLER_I, Constants.THETA_CONTROLLER_D, kThetaControllerConstraints);
-        // thetaController.enableContinuousInput(-Math.PI, Math.PI);
-
-        // // everything below had m_robotDrive replaced with userSwerveDrive
-
-        // // TODO add new methods + test
-        // SwerveControllerCommand swerveControllerCommand =
-        // new SwerveControllerCommand(
-        //     trajectory,
-        //     userSwerveDrive.m_swerveDrive::getPose, // Functional interface to feed supplier
-        //     userSwerveDrive.m_swerveDrive.kinematics,
-        //     // Position controllers
-        //     new PIDController(Constants.X_CONTROLLER_P, Constants.X_CONTROLLER_I, Constants.X_CONTROLLER_D),
-        //     new PIDController(Constants.Y_CONTROLLER_P, Constants.Y_CONTROLLER_I, Constants.Y_CONTROLLER_D),
-        //     thetaController,
-        //     userSwerveDrive.m_swerveDrive::setModuleStates,
-        //     userSwerveDrive.m_swerveDrive);
-
-        // // Reset odometry to the starting pose of the trajectory.
-        // userSwerveDrive.m_swerveDrive.resetOdometry(trajectory.getInitialPose());
-
-        // // Run path following command, then stop at the end.
-        // return swerveControllerCommand.andThen(() -> userSwerveDrive.m_swerveDrive.driveTrain(0, 0, 0));
-        // // changed from .tankDriveVolts(0, 0));
 
         return null;
     }
